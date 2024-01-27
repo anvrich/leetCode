65667786f7d1711c6e5a60976e8540138667cc75
anvrich <anvrich7000@gmail.com>
2024-01-27 22:46:38 +0300

    Single Number

---
+++ Easy/src/SingleNumber.java
@@ -0,0 +1,15 @@
+public class SingleNumber {
+    public static void main(String[] args) {
+    int[] nums = {2,2,1};
+    int uniqueNumber = singleNumber(nums);
+    System.out.println("Уникальный элемент: " + uniqueNumber);
+
+}
+public static int singleNumber(int[] nums) {
+    int result = 0;
+    for (int n : nums) {
+        result ^= n;
+    }
+    return result;
+    }
+}
