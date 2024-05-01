c6deb4d09b87cfeda9b8bd1eaa6dc298e6840596
anvrich <anvrich7000@gmail.com>
2024-05-01 11:30:05 +0300

    .

--- Easy/src/solve/TwoSum/C/TwoSum.h
+++ Easy/src/solve/TwoSum/C/TwoSum.h
@@ -1,14 +1,17 @@
-int* twoSum(int* nums, int len, int target) {
-    int* result = malloc(2 * sizeof(int));
 
-    for (int i = 0; i < len; ++i) {
-        for (int j = i+1; j < len; ++j) {
-            if(nums[i] + nums[j] == target){
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = malloc(2 * sizeof(int));
+    *returnSize = 2;
+
+    for (int i = 0; i < numsSize; ++i) {
+        for (int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] + nums[j] == target) {
                 result[0] = i;
                 result[1] = j;
                 return result;
             }
         }
     }
+
     return NULL;
 }
