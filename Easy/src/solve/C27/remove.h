f34efe93d8fe335cad30bbd2881f58274f794df2
anvrich <anvrich7000@gmail.com>
2024-05-30 15:35:32 +0500

    solution 27

---
+++ Easy/src/solve/C27/remove.h
@@ -0,0 +1,11 @@
+
+int removeElement(int nums[], int numsSize, int val) {
+    int i, j;
+    for (i = 0, j = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[j++] = nums[i];
+        }
+    }
+    return j;
+}
+
