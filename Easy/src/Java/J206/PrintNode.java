4b6ccab111debeea6910d8d6eed84157eec396ab
anvrich <anvrich7000@gmail.com>
2024-07-29 20:50:54 +0500

    206. Reverse Linked List

---
+++ Easy/src/Java/J206/PrintNode.java
@@ -0,0 +1,14 @@
+package Java.J206;
+
+import org.w3c.dom.Node;
+
+public class PrintNode {
+    public static void print(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val + " -> ");
+            cur = cur.next;
+        }
+        System.out.println("null");
+    }
+}
